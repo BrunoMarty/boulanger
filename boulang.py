35d93a2fa106dd384a8f6a56459329176012b887
bruno <bruno.marty4@gmail.com>
2016-12-27 22:57:39 +0100

    Boulanger

--- boulang.py
+++ boulang.py
@@ -1,7 +1,7 @@
 
 e=[200,100,50,20,10,5,2,1]
 
-s=750
+s=765
 i=0
 
 while i<=(len(e)-1):
@@ -11,8 +11,7 @@ while i<=(len(e)-1):
 	while s >= e[i]:
 		j = j+1
 		s = s - e[i]
-	print(e[6]/100)
-	#print (j)
-	# print (e[i]/100)
-	# print (" ")
+	print (j)
+	print float(e[i]/100.0)
+	print (" ")
 	i = i+1
